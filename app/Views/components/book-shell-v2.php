567ad5760e901124e98dc3cffcb8a692413edc68
Archon Project Local <archon-local@localhost>
2026-08-23 05:47:12 +0500

    Lower mobile cover logo

--- app/Views/components/book-shell-v2.php
+++ app/Views/components/book-shell-v2.php
@@ -1,4 +1,4 @@
-<link rel="stylesheet" href="/assets/css/book-preview.css?v=20260823-mobile12">
+<link rel="stylesheet" href="/assets/css/book-preview.css?v=20260823-mobile13">
 
 <?php require __DIR__ . '/book-welcome.php'; ?>
 
